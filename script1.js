493280dd715ca19c7ea161f3b9d5fb640be20e4d
David Gadyan <davidgadyan92@gmail.com>
2020-05-02 14:23:58 +0400

    JavaScript syntax basics

---
+++ script1.js
@@ -0,0 +1,51 @@
+// const add = (a, b) => {
+//   // The inside of the function
+//   const c = a + b;
+//   return c;
+// }
+
+// console.log(add(4, 6)); // 10
+
+
+function add(a, b) {
+  // The inside of the function
+  const c = a + b;
+  return c;
+}
+
+console.log(add(4, 6)); // 10
+
+
+var number=5; // declaring varibale
+
+/*
+hey whats'up
+
+*/
+
+
+/*
+
+we can declare variable with 3 keywords
+
+var    -- global scope definition
+let	   -- local scope definition
+const  -- variable cannot be later changed
+
+*/
+
+var a; //declaring
+var b=2; //assigning
+
+b ++; // increment by one
+
+b += 3; // add 3 to b
+b -= 2; // minus 2 from b
+
+b *= 2; //multiply by 2 and assign back
+
+b /= 2; // divide by 2 and assogn back
+
+var myString = 'you are a cool "person"';
+var myString = "you are a cool \"person\"";
+console.log(myString);
